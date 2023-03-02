3baf34ca364208202000867969086f3a7697c039
TuanAnnnn <lytuanan1911@gmail.com>
2023-03-02 08:28:34 +0700

    test commit

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace Pharmacy
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            /*
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -25,6 +26,7 @@ namespace Pharmacy
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            */
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pharmacy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,46 @@ namespace Pharmacy.Controllers
 {
     public class HomeController : Controller
     {
+        PharmacyEntities _db = new PharmacyEntities();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult getMenu()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var v = from t in _db.Menus
+                    where t.hide == true
+                    orderby t.order ascending
+                    select t;
+            return PartialView(v.ToList());
         }
 
-        public ActionResult Contact()
+        public ActionResult getFooter()
         {
-            ViewBag.Message = "Your contact page.";
+            var v = from t in _db.Footers
+                    where t.hide == true
+                    orderby t.column ascending, t.order ascending
+                    select t;
+            return PartialView(v.ToList());
+        }
 
-            return View();
+        public ActionResult getPopularProduct()
+        {
+            var v = (from p in _db.Products
+                     where p.hide == true
+                     orderby p.purchase descending
+                     select p).Take(6);
+            return PartialView(v.ToList());
+        }
+
+        public ActionResult getNews()
+        {
+            var v = (from n in _db.News
+                     where n.hide == true
+                     orderby n.datebegin descending, n.order ascending
+                     select n).Take(3);
+            return PartialView(v.ToList());
         }
     }
 }
