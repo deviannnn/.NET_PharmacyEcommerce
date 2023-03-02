66116583311490d6b87713775299c3ea78b5604a
TuanAnnnn <lytuanan1911@gmail.com>
2023-03-02 08:35:25 +0700

    commit getMenu

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,10 +21,6 @@ namespace Pharmacy.Controllers
                     where t.hide == true
                     orderby t.order ascending
                     select t;
-            var z = from t in _db.Menus
-                    where t.hide == true
-                    orderby t.order ascending
-                    select t;
             return PartialView(v.ToList());
         }
 
